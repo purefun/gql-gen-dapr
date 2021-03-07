c4d1bb581eb4cddc82e6e944d40839f27cde6f70
Nil Lin <nil@lin.am>
2021-03-07 10:48:46 +0800

    feat: generator out buffer

--- generator/generator.go
+++ generator/generator.go
@@ -1,6 +1,8 @@
 package generator
 
 import (
+	"bytes"
+
 	"github.com/purefun/gql-gen-dapr/generator/templates"
 	"github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
@@ -9,13 +11,11 @@ import (
 type Generator struct {
 	PackageName string
 	Sources     []*ast.Source
-}
-
-func NewSource(name, schemaString string) *ast.Source {
-	return &ast.Source{Name: name, Input: schemaString}
+	Out         *bytes.Buffer
 }
 
 func (g *Generator) Generate() (string, error) {
+	g.Out = &bytes.Buffer{}
 	schema, err := gqlparser.LoadSchema(g.Sources...)
 	if err != nil {
 		return "", err
@@ -33,9 +33,19 @@ func (g *Generator) Generate() (string, error) {
 		return "", tplErr
 	}
 
-	return out, nil
+	g.WriteOut(out)
+
+	return g.Out.String(), nil
+}
+
+func NewSource(name, schemaString string) *ast.Source {
+	return &ast.Source{Name: name, Input: schemaString}
 }
 
 func (g *Generator) AddSource(name, content string) {
 	g.Sources = append(g.Sources, NewSource(name, content))
+}
+
+func (g *Generator) WriteOut(o string) {
+	g.Out.WriteString(o)
 }
